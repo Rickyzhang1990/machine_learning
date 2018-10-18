e59fc72f3a319cfdde2716ca684ebcdb2fd60897
chaozhang <fredzhang1990@163.com>
2018-10-18 17:16:29 +0800

    Update preprocessing.py

--- Play_ML/preprocessing.py
+++ Play_ML/preprocessing.py
@@ -23,4 +23,19 @@ class StandardScaler:
 class MinMaxScaler:
   
   def __init__(self):
-    self
+    self.min_ = None 
+    self.max_ = None 
+  
+  def fit(self,X):
+    assert  X.ndim == 2 """只处理二维数组"""
+    self.min_ = np.array([np.min(X[:,i] for i in range(X.shape[1])])
+    self.max_ = np.array([np.max(X[:,i] for i in range(X.shape[1])])
+    return self 
+                          
+  def transform(self , x):
+    assert x.shape[1] == self.min_.shape[1];"""输入检验数据集的特征数与最大值最小值的数目相等"""                     
+    assert self.min_ is not None and self.max_ is not None;"""最大值最小值矩阵经过fit过程，不为空值""" 
+    res_x = np.empty(shape = x.shape ,dtype = float)
+    for col in range(x.shape[1]):
+      res_x[:,col] = (x[:,col] - self.min_[:,col])/(self.max_[:,col] - self.min_[:,col])
+    return rex_x
