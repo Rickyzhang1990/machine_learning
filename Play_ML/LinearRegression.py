2d9b1c35ce57e72ebdf09f9cc7a87923638751a7
chaozhang <fredzhang1990@163.com>
2018-10-22 22:22:44 +0800

    Create LinearRegression.py

---
+++ Play_ML/LinearRegression.py
@@ -0,0 +1,71 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+class SimpleLinearRegression1:
+  
+  def__inint__(self):
+    self.a_ = None 
+    self.b_ = None 
+    
+  def fit(self , x_train , y_train):
+    assert x_train.ndim == 1 ,\
+    assert len(x_train) == len(y_train),\
+    
+    x_mean = np.mean(x_train)
+    y_mean = np.mean(y_train)
+    
+    num ,d = 0.0,0.0
+    for x ,y in zip(x_train ,y_train):
+      num += (x - x_mean)*(y-y_mean)
+      d += (x-x_mean)**2
+    
+    self.a_ = num/d
+    self.b_ = y_mean = self.a_ * x_mean
+    return self 
+  def predict(self , x_predict):
+  """给定的预测数据集x_predict ,返回便是x_predict的结果向量""""
+  assert x_predict.ndim == 1 
+  assert self.a_ is not None and self.b_ is not None 
+  
+  return np.array([self._predict(x) for x in x_predict])
+  
+  def _predict(self , x_num):
+    
+    return self.a_ * x_num + self.b_
+  def __repr__(self):
+    return "SimpleLinearRegression()"
+    
+class SimpleLinearRegression2:
+  
+  def__inint__(self):
+    self.a_ = None 
+    self.b_ = None 
+    
+  def fit(self , x_train , y_train):
+    assert x_train.ndim == 1 ,\
+    assert len(x_train) == len(y_train),\
+    
+    x_mean = np.mean(x_train)
+    y_mean = np.mean(y_train)
+    
+#   num ,d = 0.0,0.0
+#   for x ,y in zip(x_train ,y_train):
+#    num += (x - x_mean)*(y-y_mean)
+#    d += (x-x_mean)**2
+    num = (x_train - y_mean).dot(y_train - y_mean)
+    d = (x_train - x_mean).dot(x_train - x_mean)
+    self.a_ = num/d
+    self.b_ = y_mean = self.a_ * x_mean
+    return self 
+  def predict(self , x_predict):
+  """给定的预测数据集x_predict ,返回便是x_predict的结果向量""""
+  assert x_predict.ndim == 1 
+  assert self.a_ is not None and self.b_ is not None 
+  
+  return np.array([self._predict(x) for x in x_predict])
+  
+  def _predict(self , x_num):
+    
+    return self.a_ * x_num + self.b_
+  def __repr__(self):
+    return "SimpleLinearRegression()"
