fa6f8745eea63bde403afaacafb21fef35ca5faf
chaozhang <fredzhang1990@163.com>
2018-10-25 21:49:08 +0800

    Update LinearRegression.py

--- Play_ML/LinearRegression.py
+++ Play_ML/LinearRegression.py
@@ -72,3 +72,27 @@ class SimpleLinearRegression2:
     return self.a_ * x_num + self.b_
   def __repr__(self):
     return "SimpleLinearRegression()"
+  
+  
+  
+  class LinearRegression:
+    """多元线性回归:正规方程法"""
+      def __init__(self):
+        self.coef_ = None 
+        self.inter_ = None
+        self._theta = None 
+      
+      def fit_normal(self , X_train , y_train):
+        """正规方程法求参数"""
+        assert X_train.shape[0] == y_train.shape[0] 
+        X_b = np.hstack([np.ones(len(X_train),1)), X_train])
+        self._theta = np.linalg.inv(X_b.T.dot(X_b)).dot(y_train)
+        self.inter_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+        return self 
+      def predict(self , X_predict):
+        x_b = np.hstack([np.ones((len(X_predict),1)),X_predict])
+        return X_b.dot(self._theta)
+      def __repr__(self):
+        return "LinearRegression"
+                                 
